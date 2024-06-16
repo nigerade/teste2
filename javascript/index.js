6b2f7c1818975b2601d158078456f3ec5f8d8414
nigerade <nicholasgerade@gmail.com>
2024-06-16 17:46:23 -0300

    antes de tentar colocar o multiplayer

--- javascript/index.js
+++ javascript/index.js
@@ -1,154 +1,77 @@
-// dont start the game and timer if game is not started yet
+// don't start the game and timer if game is not started yet
 let isGameStarted = true;
 
 // put your questions in array with JSON object
-// do not put 0 to ID => start from 1 and more // Make sure each ID is uniqe
+// do not put 0 to ID => start from 1 and more // Make sure each ID is unique
 // id will help us to track which question we are working on.
 let questions = [
   {
     id: 1,
     question: "Qual a cor do céu?",
-    answer: "azul",
+    answers: ["azul"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 2,
     question: "Se você congelar água, o que você obtém?",
-    answer: "gelo",
+    answers: ["gelo", "ice"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 3,
     question: "Qual estado é famoso por Hollywood?",
-    answer: "Califórnia",
+    answers: ["Califórnia", "California"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 4,
     question: "Quantos pares de asas as abelhas têm?",
-    answer: "Dois",
+    answers: ["Dois", "2"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 5,
     question: "Onde está a Grande Pirâmide de Gizé?",
-    answer: "Egito",
+    answers: ["Egito", "Egypt"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 6,
     question: "Qual é o maior planeta do nosso sistema solar?",
-    answer: "Júpiter",
+    answers: ["Júpiter", "Jupiter"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 7,
     question: "Qual é a capital da França?",
-    answer: "Paris",
+    answers: ["Paris"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 8,
-    question: "Qual é o símbolo químico da água?",
-    answer: "H2O",
+    question: "Quem fez o filme O Exterminador do Futuro?",
+    answers: ["Arnold Schwarzenegger"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 9,
-    question: "Em que ano o Titanic afundou?",
-    answer: "1912",
+    question: "Qual a empresa que o Edgar tanto ama?",
+    answers: ["Unilever"],
     isAnswered: false,
     userInput: "",
   },
   {
     id: 10,
-    question: "Qual é o menor país do mundo?",
-    answer: "Cidade do Vaticano",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 11,
-    question: "Qual é a substância natural mais dura da Terra?",
-    answer: "Diamante",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 12,
-    question: "Quantos continentes existem?",
-    answer: "Sete",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 13,
-    question: "Quem escreveu 'Romeu e Julieta'?",
-    answer: "William Shakespeare",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 14,
-    question: "Qual é o ingrediente principal do sushi?",
-    answer: "Arroz",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 15,
-    question: "Quantos dentes tem um humano adulto?",
-    answer: "32",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 16,
-    question: "Qual é a montanha mais alta do mundo?",
-    answer: "Monte Everest",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 17,
-    question: "Qual planeta é conhecido como o Planeta Vermelho?",
-    answer: "Marte",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 18,
-    question: "Qual é o maior oceano da Terra?",
-    answer: "Pacífico",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 19,
-    question: "Quantos ossos há no corpo humano?",
-    answer: "206",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 20,
-    question: "Qual é a capital do Japão?",
-    answer: "Tóquio",
-    isAnswered: false,
-    userInput: "",
-  },
-  {
-    id: 21,
-    question: "Quem fez o filme Terminador do Futuro?",
-    answer: "Arnold Schwarzenegger",
+    question: "O que o Vitão mais ama Dar?",
+    answers: ["Aula"],
     isAnswered: false,
     userInput: "",
   },
@@ -159,7 +82,7 @@ let currentQuestion = 0;
 // Score
 let score = 0;
 
-// coundown game time second
+// countdown game time second
 let time = 60;
 let timeID = 0;
 
@@ -183,7 +106,7 @@ inpUser.addEventListener("keypress", (e) => {
 });
 
 const displayNextQuestion = () => {
-  // get unAnswered awaiting questions
+  // get unanswered awaiting questions
   const nextQuestion = questions.filter((question) => !question.isAnswered);
   // if all questions answered go to result page
   if (!nextQuestion || nextQuestion.length < 1) {
@@ -191,7 +114,7 @@ const displayNextQuestion = () => {
   }
 
   // Random question
-  // display random question if unAnswered questions are more then 1
+  // display random question if unanswered questions are more than 1
   if (nextQuestion.length > 1) {
     const randomIndex = generateRandomNumber(nextQuestion.length);
     divQuestion.textContent = nextQuestion[randomIndex].question;
@@ -232,7 +155,7 @@ const onSubmit = () => {
     question.isAnswered = true;
 
     // update answer
-    const isAnswerCorrect = checkAnswer(question.answer, inpUser.value);
+    const isAnswerCorrect = checkAnswer(question.answers, inpUser.value);
     if (isAnswerCorrect) score++;
 
     // clear input
@@ -245,9 +168,8 @@ const onSubmit = () => {
 };
 
 // Check user answer and update the data.
-const checkAnswer = (correctAnswer, userAnswer) => {
-  if (correctAnswer.toLowerCase() === userAnswer.toLowerCase()) return true;
-  return false;
+const checkAnswer = (correctAnswers, userAnswer) => {
+  return correctAnswers.some((answer) => answer.toLowerCase() === userAnswer.toLowerCase());
 };
 
 // button click sound
